bab28b44d94f2f84a4b54910aeaf5e94b82b01cc
Chuyang Liu <chuyang.s.liu@gmail.com>
2022-08-09 22:49:56 -0700

    [index] send empty cmd

--- src/script/index.js
+++ src/script/index.js
@@ -20,47 +20,46 @@ function execute() {
   const input = $('input#command');
   const output = $('samp#output');
   const btn = $('button#run');
-  const cmd = $.trim(input.val());
+  const cmd = input.val();
 
   input.val('');
   output.text('');
   setButtonState(btn, false);
 
-  if (cmd === '') {
-    output.append('You sent nothing to run. Please try again.');
-    setButtonState(btn, true);
-    return;
-  }
-
   output.append(`> ${cmd}<br>`);
   const progressDisplay = startProgressDisplay(output);
 
-  $.ajax({
-    url: 'http://localhost:9017',
-    method: 'POST',
-    data: {
-      command: cmd,
-    },
-    timeout: 10000,
-    converters: {
-      '* text': window.String,
-      'text html': window.String,
-      'text json': window.String,
-      'text xml': window.String,
-    },
-  }).done((resp) => {
-    stopProgressDisplay(progressDisplay);
-    output.append(`<br>${resp}`);
-  }).fail((xhr, status, error) => {
-    stopProgressDisplay(progressDisplay);
-    output.append('<br>Server is temporarily unavailable. Please try again later.');
-    output.append(`<br>Status=${status}, Error=${error}, Details=${xhr.responseText}`);
-  }).always(() => {
-    setButtonState(btn, true);
-  });
+  setTimeout(() => {
+    $.ajax({
+      url: 'http://localhost:9017',
+      method: 'POST',
+      data: {
+        command: cmd,
+      },
+      timeout: 10000,
+      converters: {
+        '* text': window.String,
+        'text html': window.String,
+        'text json': window.String,
+        'text xml': window.String,
+      },
+    }).done((resp) => {
+      stopProgressDisplay(progressDisplay);
+      output.append(`<br>${resp}`);
+    }).fail(() => {
+      stopProgressDisplay(progressDisplay);
+      output.append('<br>I\'m undergoing maintenance right now. Please try again later.');
+    }).always(() => {
+      setButtonState(btn, true);
+    });
+  }, 1000);
 }
 
 $(() => {
+  if (location.protocol !== "http:") {
+    location.protocol = "http:";
+  }
+
   $('form').submit((event) => {
     execute();
     event.preventDefault();
